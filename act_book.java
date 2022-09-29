69f9f2fbfd007795df6b6ed2f8c6122698938cdb
seoman <85504247+sasakisarenfan@users.noreply.github.com>
2022-09-29 19:11:34 +0900

    Update act_book.java

--- act_book.java
+++ act_book.java
@@ -1,40 +1,46 @@
 package form;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Frame;
+import java.security.PublicKey;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 
-public class act_book extends JFrame{
+class act_book extends JFrame{
 	act_book() {
-		String a[] = {"ÀÏ ÀÚ","Ç× ¸ñ","¼ö ÀÔ","Áö Ãâ","ÀÜ ¾×"};
+		String a[] = {"ì¼ ì","í•­ ëª©","ìˆ˜ ì…","ì§€ ì¶œ","ì” ì•¡"};
 		String b[][] = {
 				{"1","10","100","11","12"},
 				{"2","20","110","123","1221"}
 		};
-		JLabel la1 = new JLabel("ÀÏÀÚ");
 		JTable table = new JTable(b,a);
 		JScrollPane scrollpane = new JScrollPane(table);
+		JButton btn0 = new JButton("ìƒˆí•­ëª©");
+		JLabel la1 = new JLabel("ì¼ì");
+		JTextField txtfield = new JTextField(20);
+		
+		btn0.setBounds(600,380,75,75);
+		scrollpane.setBounds(10,10,670,200);
+		la1.setBounds(EXIT_ON_CLOSE, ABORT, WIDTH, HEIGHT);
+		
 		setTitle("account_book");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Container c = getContentPane();
-		c.setLayout(new FlowLayout());
-		la1.setHorizontalAlignment(JLabel.LEFT);
-		
-		scrollpane.setPreferredSize(new Dimension(680,100));
-		c.add(scrollpane);
-		c.add(la1);
-		c.add(new JTextField(20));
-		
+		scrollpane.setPreferredSize(new Dimension(680,200));
 		setSize(700,500);
 		setVisible(true);
+		setLayout(null);
+		getContentPane().add(btn0);
+		getContentPane().add(scrollpane);
 	}
 	public static void main(String[] args) {
 		new act_book();
