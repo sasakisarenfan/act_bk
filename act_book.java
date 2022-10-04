51f3dd61edb13b59088914e05608f17c35d6226b
seoman <85504247+sasakisarenfan@users.noreply.github.com>
2022-10-04 19:55:13 +0900

    Update act_book.java

--- act_book.java
+++ act_book.java
@@ -1,4 +1,4 @@
-package lesson;
+package form;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
@@ -14,6 +14,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 class act_book extends JFrame{
@@ -29,23 +30,48 @@ class act_book extends JFrame{
 		JLabel la1 = new JLabel("일자");
 		JTextField txtfield = new JTextField(20);
 		JLabel la2 = new JLabel("항목");
+		JTextField txtfield2 = new JTextField(20);
+		JLabel la3 = new JLabel("수입");
+		JTextField txtfield3 = new JTextField(20);
+		JLabel la4 = new JLabel("지출");
+		JTextField txtfield4 = new JTextField(20);
+		JButton btn1 = new JButton("저장");
+		JTextArea txtarea = new JTextArea(5,20);
 		
-		btn0.setBounds(600,380,75,75);
+		btn1.setBounds(600,380,75,75);
 		scrollpane.setBounds(10,10,670,200);
-		la1.setBounds(50,240,50,50);
-		txtfield.setBounds(80,255,100,20);
-		la2.setBounds(EXIT_ON_CLOSE, ABORT, WIDTH, HEIGHT);
+		la1.setBounds(70,240,50,50);
+		txtfield.setBounds(100,255,150,20);
+		la2.setBounds(320,240,50,50);
+		txtfield2.setBounds(350,255,150,20);
+		la3.setBounds(70,300,50,50);
+		txtfield3.setBounds(100,315,150,20);
+		la4.setBounds(320,300,50,50);
+		txtfield4.setBounds(350,315,150,20);
+		btn0.setBounds(600,300,75,75);
+		txtarea.setBounds(20,370,565,75);
 		
 		setTitle("account_book");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		scrollpane.setPreferredSize(new Dimension(680,200));
 		setSize(700,500);
 		setVisible(true);
+		setResizable(false);
+		setLocationRelativeTo(null);
 		setLayout(null);
+		scrollpane.setEnabled(false);
 		getContentPane().add(btn0);
 		getContentPane().add(scrollpane);
 		getContentPane().add(la1);
 		getContentPane().add(txtfield);
+		getContentPane().add(la2);	
+		getContentPane().add(txtfield2);
+		getContentPane().add(la3);
+		getContentPane().add(txtfield3);
+		getContentPane().add(la4);
+		getContentPane().add(txtfield4);
+		getContentPane().add(btn1); 
+		getContentPane().add(txtarea);
 	}
 	public static void main(String[] args) {
 		new act_book();
